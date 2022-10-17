d4e2803958e88f0c221ec51a1224506a54f6cb65
baradejoo <baradziejkamil@gmail.com>
2022-10-17 17:31:26 +0200

    Ap5 - added

---
+++ Lab1/Aplikacja5/Aplikacja5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aplikacj5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            const int width = 4;
+            const int height = 6;
+
+            Console.Write("Podaj cyfre: ");
+            var x = Console.ReadLine();
+            var ch = x[0]; // przypadek kiedy ktos poda 2cyfrowa liczbe
+
+            Console.WriteLine(" {0} ", new string(ch, width));
+            for (int i = 0; i < height; i++)
+                Console.WriteLine("{0}{1}{0}", ch, new string(' ', width));
+            Console.WriteLine(" {0} ", new string(ch, width));
+        }
+    }
+}
