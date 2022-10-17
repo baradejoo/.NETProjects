8447da2378023bc7a80bd7ca4249b8e3434570ec
baradejoo <baradziejkamil@gmail.com>
2022-10-17 17:25:45 +0200

    Ap4 - addded

---
+++ Lab1/Aplikacja4/Aplikacja4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cwiczenie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj 3 liczby (oddzielone 1x spacja): ");
+            var input = Console.ReadLine().Split(' ');
+            var a = Convert.ToInt32(input[0]);
+            var b = Convert.ToInt32(input[1]);
+            var c = Convert.ToInt32(input[2]);
+            Console.WriteLine($"{c} x {b} x {a} = {a * b * c}");
+        }
+    }
+}
