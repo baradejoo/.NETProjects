da5fbd0494130d10f6b0a397418dcfb642644dc9
baradejoo <baradziejkamil@gmail.com>
2022-10-17 17:54:54 +0200

    Added Ap9

---
+++ Lab1/Aplikacja9_Kamil/Aplikacja9_Kamil/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("JetBrains Rider ma narzędzie ILViewer\n Jego output z tego pliku (Program.cs)\n skopiowałem do Rider_IL_Viewer.txt\n             *");
+
