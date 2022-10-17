b9d721669b75d06c1fd367f54ff6a3fca2072343
baradejoo <baradziejkamil@gmail.com>
2022-10-17 17:40:29 +0200

    Added Ap7

---
+++ Lab1/Aplikacja7/Aplikacja7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aplikacja7
+{
+    class Program
+    {
+        static double toFahrenheit(double celsius) => celsius * 1.8 + 32.0;
+
+        static double toKelvin(double celsius) => celsius + 273;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj temperature w stopniach Celsjusza: ");
+            var celsius = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}°C = {1}°F = {2}K", celsius, toFahrenheit(celsius), toKelvin(celsius));
+        }
+    }
+}
