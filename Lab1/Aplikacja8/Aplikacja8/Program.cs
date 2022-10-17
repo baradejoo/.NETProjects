8725e022be19dac0ce91da1a3f1184827fc1dfba
baradejoo <baradziejkamil@gmail.com>
2022-10-17 17:43:25 +0200

    Added Ap8

---
+++ Lab1/Aplikacja8/Aplikacja8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aplikacja8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 > 0 && num2 < 0 || num1 < 0 && num2 > 0);
+
+            // xor, ale zero jest uznawane za dotatnia
+            Console.WriteLine(num1 >= 0 ^ num2 >= 0);
+        }
+    }
+}
