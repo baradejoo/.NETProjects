c9b6e774169bbf77f23e276c98e0a9b408b4ef73
baradejoo <baradziejkamil@gmail.com>
2022-11-01 21:29:12 +0100

    Exercise 2 - Done

---
+++ Lab2/ProjektLab2/ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ClassLibrary1
+{
+    namespace TestLib1
+    {
+        public class Class
+        {
+            public Class()
+            { }
+            public string Version
+            {
+                get
+                {
+                    Assembly assembly = Assembly.GetAssembly(GetType());
+                    return assembly.GetName().Version.ToString();
+                }
+            }
+        }
+    }
+
+}
