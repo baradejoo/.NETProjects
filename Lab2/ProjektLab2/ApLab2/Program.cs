2ca0cb7f0875a643ac08536007e3dca86eb6d834
baradejoo <baradziejkamil@gmail.com>
2022-11-01 22:00:05 +0100

    the whole exercise 2 done

--- Lab2/ProjektLab2/ApLab2/Program.cs
+++ Lab2/ProjektLab2/ApLab2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1.TestLib1;
 
 namespace ApLab2
 {
@@ -10,6 +11,9 @@ namespace ApLab2
     {
         static void Main(string[] args)
         {
+            Class c = new Class();
+            Console.WriteLine("Wersja: {0}", c.Version);
+            Console.ReadKey();
         }
     }
 }
