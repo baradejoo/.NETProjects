67065a0dcc9433e907e03def22a188810d59e4b0
baradejoo <baradziejkamil@gmail.com>
2022-11-28 23:44:06 +0100

    Ex5 added

---
+++ Lab5/Aplikacja5/Aplikacja5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Aplikacja5
+{
+    class Program
+    {
+ 
+        [DllImport("msvcrt.dll")]
+        private static extern void puts(string str); 
+
+        [DllImport("msvcrt.dll")]
+        private static extern void _flushall();
+
+   
+        static void Main(string[] args)
+        {
+            puts("Funkcja puts !@#$%");
+            Console.ReadKey();
+        }
+    }
+}
